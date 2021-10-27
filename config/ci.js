fe5f845bc3622d11d7a67f95bd5aa1528d4c4ca7
Matias Marin <matiasmarin45@gmail.com>
2021-10-27 12:08:17 -0300

    Deploy for travis

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+  '99408261793-v4nk0dvgbhps7upfoo9ud0lbej7jpcnr.apps.googleusercontent.com',
+googleClientSecret: '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',
+mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
---
+++ config/dev.js
@@ -0,0 +1,12 @@
+// Don't be like me and commit this file!
+// These keys have been disabled, but remain here for reference purposes.
+module.exports = {
+  googleClientID:
+    '99408261793-v4nk0dvgbhps7upfoo9ud0lbej7jpcnr.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  mongoURI: 'mongodb://user:bIMtCIMDP5r9GFQQ@nodeadvanced-shard-00-00.vffp3.mongodb.net:27017,nodeadvanced-shard-00-01.vffp3.mongodb.net:27017,nodeadvanced-shard-00-02.vffp3.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-oi5pk4-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAIWSXFHRM7F6Z3NWQ',
+  secretAccessKey: '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'
+};
